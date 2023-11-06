a2fee92f3f8222353e590d0ca3b82cbc5493deb4
Mauricio <89703149+Mauricio-Miranda@users.noreply.github.com>
2023-11-05 22:16:34 -0300

    primera carga

---
+++ router-api.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'libs/Router.php';
+require_once 'api.controller/game.controller.php';
+require_once 'api.controller/coments.controller.php';
+
+// creo el router
+$router = new Router();
+
+// tabla de ruteo
+$router->addRoute('juegos', 'GET', 'GameController', 'getAll');
+$router->addRoute('juegos/:ID', 'GET', 'GameController', 'getOne');
+$router->addRoute('juegos/:ID', 'DELETE', 'GameController', 'remove');
+$router->addRoute('juegos', "POST", 'GameController', 'addGame');
+$router->addRoute('juegos/:ID', "PUT", 'GameController', 'updateGame');
+
+
+// rutea
+$resource = $_GET['resource'];
+$method = $_SERVER['REQUEST_METHOD'];
+$router->route($resource, $method);
+
