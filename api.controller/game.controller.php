940f657f3cb5cdb46f56e8bebfe61fc65749e247
Mauricio Miranda <mauriciotresa@gmail.com>
2023-11-10 02:38:12 -0300

    no funciona orden, filtro ni paginacion

--- api.controller/game.controller.php
+++ api.controller/game.controller.php
@@ -13,8 +13,103 @@ class GameController {
     }
 
     public function getAll($params = null) {
-        $games = $this->model->getAllGames();
+
+        // Parametro de ordenamiento - campo por defecto 'id' - se puede ordenar por 'id', 'juego' o 'desarrollador'
+
+        if (isset ($_GET['order'])){
+            $orderBy = $_GET['order'];
+            if($orderBy !=='id' && $orderBy !=='juego' && $orderBy !== 'desarrollador'){
+                $orderBy = 'id';
+            }
+        }
+
+        // Direccion del ordenamiento - Direccion por defecto 'ascendente' - puede ser ascendente o descendente
+
+        if (isset($_GET['dir'])){
+            $direction = $_GET['dir'];
+            if($direction !=='asc' && $direction !== 'des'){
+                $direction = 'asc';
+            }
+        }
+
+        // Parametro de filtro - por defecto 'null' sin finltro - puede filtrarce por 'desarrollador'
+
+        if (isset($_GET['filterBy'])){
+            $filterBy = $_GET['filterBy'];
+            if ($filterBy !== 'desarrollador'){
+                $filterBy = null;
+            }
+        }
+        
+        // Paginacion
+
+        if (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0){
+            $page = $_GET['page'];
+        } else{        
+            $page = 1;
+        }
+
+        if (isset($_GET['limit']) && is_numeric($_GET['limit']) && $_GET['limit'] > 0){
+            $limit = $_GET['limit'];
+        } else{        
+            $limit = 10;
+        }
+
+        if (isset($_GET['paginate']) && $_GET['paginate'] === 'true') {
+            // Calcular el offset para la paginación
+            $offset = ($page - 1) * $limit;
+            $games = $this->model->getAllGames($orderBy, $direction, $filterBy, $offset, $limit);
+        } else {
+            // Sin paginación, obtener todos los juegos
+            $games = $this->model->getAllGames($orderBy, $direction, $filterBy, null, null);
+        }
+
+
+        //$games = $this->model->getAllGames();
         $this->view->response($games);
         
+    }
+
+    public function getOne($params = null){
+        $id = $params[":ID"];
+        $game = $this->model->getGame($id);
+
+        if ($game)
+            $this->view->response($game);
+        else
+        $this->view->response("game id=$id not found", 404);
+    }
+
+    public function addGame(){
+        //$this->data = file_get_contents("php://input");
+        $game = $this->getData();
+
+        if($game){
+            $this->model->addGame($game->juego, $game->texto,$game->imagen,$game->desarrollador);
+            $this->view->response("Game successfully added.",200);
+        }
+        else{
+            $this->view->response("Game not added, an error has occurred.",404);
+        }
+
+    }
+
+    public function updateGame($params = null) {
+        //$this->data = file_get_contents("php://input");
+        
+        $id = $params[':ID'];
+        $data = $this->getData();
+        
+        $game = $this->model->getGame($id);
+        if ($game) {
+            $this->model->updateGame($id, $data->juego, $data->texto,$data->imagen,$data->desarrollador);
+            $this->view->response("El juego fue modificado con exito.", 200);
+        } else
+            $this->view->response("El juego con el id={$id} no existe", 404);
+    }
+
+    private function getData(){
+        $this->data = file_get_contents("php://input");
+        return json_decode($this->data);
     }
 }
--- api.model/game.model.php
+++ api.model/game.model.php
@@ -9,39 +9,53 @@ class GamesModel{
         $this->db = $this-> conect();
         
     }
-
-
     // Abre la coneccion con la base de Datos
     function conect(){
         $db = new PDO ('mysql:host=localhost;' . 'dbname=tpeweb2; charset=utf8', 'root', '');
         return $db;
     }
 
-    // Obtener todos los juegos de la DB
-    function getAllGames(){
-        
-        // 2.- Enviamos una consulta y nos devuelve el resultado.-
-        // SELECT * FROM juegos
-       // $query = $this->db -> prepare ('SELECT * FROM juegos');  // Son dos sub pasos a) Preparar la consulta y
+    /* Obtener todos los juegos de la DB
+    //function getAllGames(){        
+       
         $query = $this->db -> prepare ('SELECT juegos.*, desarrolladores.company AS company FROM juegos
-        INNER JOIN desarrolladores ON juegos.desarrollador = desarrolladores.id_desarrollador');
-        $query-> execute();                                      // b) Ejecutarla
-        /*
-        SELECT juegos.*, desarrolladores.company AS company FROM juegos
         INNER JOIN desarrolladores ON juegos.desarrollador = desarrolladores.id_desarrollador
+        WHERE');
+        $query-> execute();           
+        $games = $query -> fetchAll(PDO::FETCH_OBJ);                       
+
+        return $games;  */
+
+    function getAllGames($orderBy, $direction, $filterBy, $offset, $limit) {
+        $query = $this->db->prepare(" SELECT juegos.*, desarrolladores.company AS company FROM juegos
+                                      INNER JOIN desarrolladores ON juegos.desarrollador = desarrolladores.id_desarrollador
+                                      WHERE (? IS NULL OR desarrolladores.company = ?)
+                                      ORDER BY $orderBy $direction LIMIT ?, ?");
+
+        $query->execute([$filterBy, $filterBy, $offset, $limit]);
+        $games = $query->fetchAll(PDO::FETCH_OBJ);   
+        return $games;
+    }
 
 
 
 
-        'SELECT juegos.*, desarrolladores.* FROM juegos AS juegos INNER JOIN desarrolladores AS desarrolladores ON juegos.desarrollador = desarrolladores.id_desarrollador');
-        */ 
+    function getGame($id){
+        $query = $this->db->prepare('SELECT * FROM juegos WHERE id = ?');
+        $query->execute([$id]);
+        $game = $query -> fetch(PDO::FETCH_OBJ);
+        return $game;
+    }
 
-        // 3.- Obtengo la repuesta de la DB
-        $games = $query -> fetchAll(PDO::FETCH_OBJ);            // Obtengo un arreglo con TODOS los juegos        
+    public function addGame($nombre, $texto, $imagen, $desarrollador){ 
+        $query = $this->db -> prepare ('INSERT INTO juegos (juego, texto, imagen, desarrollador) VALUE (?, ?, ?, ?)');  
+        $query->execute([$nombre, $texto, $imagen, $desarrollador]);           
+    
+    }
 
-        // 4.- Cerramos la coneccion.-   En PDO  NO HAY QUE CERRAR LA CONEXION
-
-        return $games;  // devuelvo los juegos
-
+    function updateGame($id, $juego, $texto, $imagen, $desarrollador){
+        $query = $this->db -> prepare('UPDATE juegos SET juego =?, texto=?, imagen=?, desarrollador=? WHERE id = ?');
+        $query-> execute([$juego, $texto, $imagen, $desarrollador, $id]);
+        
     }
 }
